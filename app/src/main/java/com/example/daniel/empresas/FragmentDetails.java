94f58440e325351c346b2b5f70b46b7890c500d8
Daniel <dss.striker@gmail.com>
2017-12-14 23:32:50 +0000

    3º- Commit

---
+++ app/src/main/java/com/example/daniel/empresas/FragmentDetails.java
@@ -0,0 +1,30 @@
+package com.example.daniel.empresas;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentDetails extends Fragment {
+
+
+    public FragmentDetails() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_details_fragment);
+        return textView;
+    }
+
+}
