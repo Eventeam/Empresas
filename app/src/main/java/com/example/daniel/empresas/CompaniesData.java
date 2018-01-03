22759b84dd8d2d86a89672aaaa52a2edc7e3b900
Daniel <dss.striker@gmail.com>
2018-01-03 11:33:58 +0000

    5º- Commit

---
+++ app/src/main/java/com/example/daniel/empresas/CompaniesData.java
@@ -0,0 +1,160 @@
+package com.example.daniel.empresas;
+
+/**
+ * Created by Daniel on 19-12-2017.
+ */
+
+public class CompaniesData {
+    static final String RESPONSE = "[\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"Sem Ir\",\n" +
+            "\t\"Categoria\": \"Energias Renováveis\",\n" +
+            "\t\"Foto/Logo\": \"http://www.semir.pt/images/entries/700x240/logo_700x240.png\",\n" +
+            "\t\"Descrição\": \"A SEM IR Lda, é uma Empresa de Engenharia e Manutenção Indústrial e Residencial com sede em Santarém e formada por profissionais qualificados e de elevada experiência, conhecimento e formação. Está vocacionada para a prestação de serviços aos seus clientes com elevada Qualidade e grande sentido de Responsabilidade.\",\n" +
+            "\t\"Telefone\": \"+351243469199\",\n" +
+            "\t\"Email\": \"geral@semir.pt\",\n" +
+            "\t\"Web\": \"http://www.semir.pt/index.html\",\n" +
+            "\t\"Localização\": [\"39.306386\", \"-8.734095\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/SEMIRLDA/\",\n" +
+            "\t\"LinkedIn\": \"\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"https://plus.google.com/b/108629657151859728538/108629657151859728538/posts?gmbpt=true&pageId=108629657151859728538&hl=pt-PT\",\n" +
+            "\t\"Youtube\": \"https://www.youtube.com/channel/UCzbg9DYjSxl9OmDIbSHGvGw\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"Galrão Renováveis\",\n" +
+            "\t\"Categoria\": \"Energias Renováveis\",\n" +
+            "\t\"Foto/Logo\": \"http://www.galraorenovaveis.pai.pt/img/0221/691.png\",\n" +
+            "\t\"Descrição\": \"A Galrão Renováveis, Lda. é uma empresa portuguesa que surge em consequência das exigências do mercado, tendo como base o conhecimento e experiência de mercado adquiridos ao longo de mais de 25 anos. Apresentamo-nos no mercado do comércio e instalação, através do fornecimento não só de equipamentos, mas essencialmente de soluções integradas e adaptadas às necessidades de cada cliente. Disponibilizamos um serviço completo nas áreas de projecto, assim como na prestação de serviços de assistência. Temos para oferecer um serviço de qualidade na área, trabalhando directamente com profissionais ou com o cliente final.\",\n" +
+            "\t\"Telefone\": \"+351210997867\",\n" +
+            "\t\"Email\": \"geral@galraorenovaveis.pt\",\n" +
+            "\t\"Web\": \"http://www.galraorenovaveis.pai.pt\",\n" +
+            "\t\"Localização\": [\"38.848474\", \"-9.319470\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/Galraorenovaveis\",\n" +
+            "\t\"LinkedIn\": \"\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"Eternal Energy Solutions by Moreme Solar\",\n" +
+            "\t\"Categoria\": \"Energias Renováveis\",\n" +
+            "\t\"Foto/Logo\": \"http://www.eternalenergysolutions.com/assets/template/images/logo.png\",\n" +
+            "\t\"Descrição\": \"A Eternal Energy Solutions by Moreme Solar, é uma empresa do grupo MOREME com mais de 30 anos de experiência no fornecimento de energia e desde 2001 no sector das energias renováveis. A Moreme assume o compromisso de encontrar as melhores soluções para satisfazer as necessidades de fornecimento de eletricidade às empresas suas clientes. A sustentabilidade da empresa e dos seus clientes, num enquadramento de proteção de pessoas e ambiente, está sempre presente nas soluções que apresentamos e nas atividades que prosseguimos.\",\n" +
+            "\t\"Telefone\": \"+351219818380\",\n" +
+            "\t\"Email\": \"info@moreme.pt\",\n" +
+            "\t\"Web\": \"http://www.eternalenergysolutions.com\",\n" +
+            "\t\"Localização\": [\"38.801749\", \"-9.250364\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/EternalEnergySolutions/\",\n" +
+            "\t\"LinkedIn\": \"\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"https://plus.google.com/112552586176945128657/posts\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"SunEnergy\",\n" +
+            "\t\"Categoria\": \"Energias Renováveis\",\n" +
+            "\t\"Foto/Logo\": \"http://www.sunenergy.pt/wp-content/uploads/2017/03/cropped-sunenergy.png\",\n" +
+            "\t\"Descrição\": \"A SunEnergy é uma marca especializada na oferta de soluções a particulares e empresas, baseada em energias renováveis com vários anos de experiência. A SunEnergy é composta por um conjunto alargado de delegações espalhadas pelo país. Conta também com uma rede de profissionais com um grande know-how adquirido ao longo de vários anos comprometidos com a satisfação dos seus clientes. Com um modelo de atuação assente numa política de proximidade e na criação de uma relação de confiança, na SunEnergy procuramos responder às necessidades dos nossos clientes, projetando e instalando sistemas de produção de eletricidade e calor adaptadas às necessidades de cada cliente. Através da implementação de sistemas energéticos limpos e eficientes para climatização, produção de água quente e geração de eletricidade com recurso a painéis solares fotovoltaicos, contribuímos para um desenvolvimento sustentável, otimizando o consumo de recursos energéticos e melhorando o bem-estar das pessoas. Na Sunenergy procuramos estar sempre na linha da frente ao nível tecnológico, através da colaboração ativa com fornecedores de referência em todo o mundo, garantindo desta forma a oferta de soluções que cumprem com os mais rigorosos padrões de qualidade e que nos permitem acrescentar valor aos nossos projetos. Com a experiência adquirida desde 2010 e o rigor que colocamos em cada um dos nossos projetos, acreditamos que a SunEnergy irá merecer a sua confiança!\",\n" +
+            "\t\"Telefone\": \"+351239700750\",\n" +
+            "\t\"Email\": \"info@sunenergy.pt\",\n" +
+            "\t\"Web\": \"http://www.sunenergy.pt\",\n" +
+            "\t\"Localização\": [\"40.236548\", \"-8.437103\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/pagina.sunenergy\",\n" +
+            "\t\"LinkedIn\": \"\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"SINERGIAE Lisboa\",\n" +
+            "\t\"Categoria\": \"Energias Renováveis\",\n" +
+            "\t\"Foto/Logo\": \"http://www.sinergiae.com/wp-content/uploads/2015/11/logotipo-SInergiae.jpg\",\n" +
+            "\t\"Descrição\": \"A SINERGIAE Lisboa Lda., foi criada em 2004. A Marca SINERGIAE tem vindo a solidificar a sua presença no mercado pautando a sua atuação por valores como a inovação, paixão, honestidade, excelência, responsabilidade social e talento. A SINERGIAE Lisboa, fundada a 13 de Outubro de 2010, descende do, já extinto, Grupo SINERGIAE. Em processo de modernização contínua e com uma gestão dinâmica, a SINERGIAE Lisboa tem vindo a desenvolver desde então elevadas competências na área das ENERGIAS RENOVÁVEIS. Nos quadros da empresa conta com uma equipa jovem, qualificada e empenhada na satisfação dos clientes e aposta na inovação e na transferência de conhecimentos. A SINERGIAE Lisboa procura as soluções mais vantajosas e integradas para que qualquer projeto possa ser realmente grandioso. Com uma forte aposta na criação e fortalecimento de parcerias, A SINERGIAE Lisboa tem capacidade para dar resposta a projetos de grande dimensão conseguindo oferecer aos seus clientes serviços chave na mão, desde o projeto até à instalação, de soluções com energias renováveis. A SINERGIAE é mais que uma marca, é uma paixão!\",\n" +
+            "\t\"Telefone\": \"+35121924556\",\n" +
+            "\t\"Email\": \"geral.lisboa@sinergiae.com\",\n" +
+            "\t\"Web\": \"http://www.sinergiae.com\",\n" +
+            "\t\"Localização\": [\"38.842734\", \"-9.370500\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/sinergiaelisboa\",\n" +
+            "\t\"LinkedIn\": \"https://pt.linkedin.com/company/grupo-sinergiae\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"JODEL\",\n" +
+            "\t\"Categoria\": \"Indústria de detergentes\",\n" +
+            "\t\"Foto/Logo\": \"http://jodel.pt/wp-content/uploads/2015/02/Logo-Jodel-web.png\",\n" +
+            "\t\"Descrição\": \"A JODEL é uma empresa de estrutura familiar que prima pela qualidade e segurança dos seus produtos. Prova disso é a sua carteira de clientes em todo o mundo. Dedica-se, desde a sua fundação em 1974, ao fabrico e distribuição de detergentes líquidos e sólidos, com marca própria e marcas da distribuição, a sua estratégia comercial está orientada para duas áreas distintas: doméstica e  industrial.\",\n" +
+            "\t\"Telefone\": \"+351263470180\",\n" +
+            "\t\"Email\": \"geral@jodel.pt\",\n" +
+            "\t\"Web\": \"http://www.jodel.pt\",\n" +
+            "\t\"Localização\": [\"39.16443843\", \"-8.92478496\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/www.jodel.pt\",\n" +
+            "\t\"LinkedIn\": \"https://www.linkedin.com/company/jodel-s.a.\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"GEPACK\",\n" +
+            "\t\"Categoria\": \"Indústria de plásticos\",\n" +
+            "\t\"Foto/Logo\": \"http://static.lvengine.net/gepack2012/interface/imgs/logo.png\", \n" +
+            "\t\"Descrição\": \"GEPACK is a private owned business, with expertise in manufacturing Premium PET bottles as a true alternative to glass. Our focus is to produce innovative glass like containers with top quality standards. Manufacturing processes are certified by ISO9001, ISO22000, HACCP, FDA compliant and a state of the art ISO6 clean room. With a wide portfolio of more than 350 shapes, GEPACK can address any project for a variety of segments - food, pharma, cosmetics or spirits. You are welcome to browse our website and feel free reach out for any doubt.\",\n" +
+            "\t\"Telefone\": \"+351263470210\",\n" +
+            "\t\"Email\": \"comercial@gepack.pt\",\n" +
+            "\t\"Web\": \"http://www.gepack.pt\",\n" +
+            "\t\"Localização\": [\"39.149714\", \"-8.901437\"],\n" +
+            "\t\"Facebook\": \"\",\n" +
+            "\t\"LinkedIn\": \"https://www.linkedin.com/company/gepack\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"Samsys\",\n" +
+            "\t\"Categoria\": \"Tecnologias de Informação\",\n" +
+            "\t\"Foto/Logo\": \"http://www.lispolis.pt/wp-content/uploads/2016/12/logo-samsys.png\", \n" +
+            "\t\"Descrição\": \"A Samsys atua na área das TI’s e integra a prestação de serviços de consultoria informática, com a missão de desenvolver soluções que ajudem as empresas a crescer.\",\n" +
+            "\t\"Telefone\": \"+351211450301\",\n" +
+            "\t\"Email\": \"geral@samsys.pt\",\n" +
+            "\t\"Web\": \"http://www.samsys.pt\",\n" +
+            "\t\"Localização\": [\"41.205316\", \"-8.527452\"],\n" +
+            "\t\"Facebook\": \"https://www.facebook.com/SAMSYS\",\n" +
+            "\t\"LinkedIn\": \"http://www.linkedin.com/company/samsys-inform-tica\",\n" +
+            "\t\"Twitter\": \"https://twitter.com/samsyspt\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"http://www.youtube.com/samsysCSI\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"Jerónimo Martins\",\n" +
+            "\t\"Categoria\": \"Comércio de alimentos\",\n" +
+            "\t\"Foto/Logo\": \"http://www.kidsclub.pt/wp-content/uploads/2016/01/logo-JMartins.png\",\n" +
+            "\t\"Descrição\": \"Vamos ao encontro das necessidades de milhões de consumidores através de uma proposta de valor assente em qualidade a preços competitivos. A nossa principal actividade é a Distribuição Alimentar que representa mais de 95% das vendas consolidadas do Grupo. Na Polónia, a cadeia de lojas alimentares Biedronka é o nosso maior negócio enquanto em Portugal, o Pingo Doce e o Recheio são líderes nos seus segmentos: retalho e grosso, respectivamente. Na Colômbia, gerimos a cadeia de lojas de proximidade Ara.\",\n" +
+            "\t\"Telefone\": \"+351217532000\",\n" +
+            "\t\"Email\": \"\",\n" +
+            "\t\"Web\": \"https://www.jeronimomartins.com/pt/\",\n" +
+            "\t\"Localização\": [\"38.760017\", \"-9.156511\"],\n" +
+            "\t\"Facebook\": \"\",\n" +
+            "\t\"LinkedIn\": \"https://www.linkedin.com/company/164533/\",\n" +
+            "\t\"Twitter\": \"\",\n" +
+            "\t\"Google+\": \"\",\n" +
+            "\t\"Youtube\": \"https://www.youtube.com/JeronimoMartins\"\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\"Nome\": \"\",\n" +
+            "\t\"Categoria\": \"Transporte e Logística\",\n" +
+            "\t\"Foto/Logo\": \"http://www.luis-simoes.pt/uploads/LSLuisSimoes%20vertical_Branco.jpg\",\n" +
+            "\t\"Descrição\": \"A Luís Simões(LS), é uma empresa de transporte e logística portuguesa com sede na povoação de Moninhos da freguesia de Loures e opera fundamentalmente na Península Ibérica. Legalmente constituída como sociedade em 1968, a história da Luís Simões inicia-se nos anos 30, quando o fundador Fernando Luís Simões começa transportar de carroça mercadorias entre Lisboa e Malveira. O grupo é propriedade da família Luís Simões. Atual presidente do grupo é José Luís Simões.\",\n" +
+            "\t\"Telefone\": \"+351219827522\",\n" +
+            "\t\"Email\": \"gabcom@luis-simoes.com\",\n" +
+            "\t\"Web\": \"http://www.luis-simoes.pt\",\n" +
+            "\t\"Localização\": [\"38.851885\", \"-9.209687\"],\n" +
+            "\t\"Facebook\": \"https://pt-pt.facebook.com/GrupoLuisSimoes/\",\n" +
+            "\t\"LinkedIn\": \"http://www.linkedin.com/company/luis-simoes-logistica-integrada-s-a-?trk=biz-companies-cym\",\n" +
+            "\t\"Twitter\": \"https://twitter.com/grupoluissimoes\",\n" +
+            "\t\"Google+\": \"https://plus.google.com/u/0/105758418530432976444\",\n" +
+            "\t\"Youtube\": \"https://www.youtube.com/user/GabComLS\"\n" +
+            "\t}\n" +
+            "]";
+}
